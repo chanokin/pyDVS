f51d28a23f347e9f986198d8f9bdc73a8ab98d6d
Garibaldi Pineda Garcia <chanokin@gmail.com>
2015-07-02 17:42:55 +0100

    Convolution opperation added

--- pyDVS/opencl_dvs.py
+++ pyDVS/opencl_dvs.py
@@ -42,7 +42,7 @@ class OpenCL_DVS():
                                              cl_array.vec.uchar4)
     self.previous_frame_gpu = cl_array.zeros(self.queue, self.array_size,\
                                              cl_array.vec.uchar4)
-    self.previous_frame_gpu.fill(cl_array.vec.make_uchar4(127))
+    self.previous_frame_gpu.fill(cl_array.vec.make_uchar4(0))
     
     self.result_gpu  = cl_array.zeros(self.queue, self.array_size, \
                                       cl_array.vec.short4)
@@ -94,20 +94,60 @@ class OpenCL_DVS():
   def kernel_str(self):      
     kernel = """
 #pragma OPENCL EXTENSION cl_amd_media_ops : enable
-//#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable
 
 __kernel void difference(__global uchar4* current, __global uchar4* previous,
                          __global uchar4* threshold, __global short4*  output){
   uint row = get_global_id(1), col = get_global_id(0);
 
-
-  if(row < IMAGE_HEIGHT && col < IMAGE_WIDTH_4){
+  //from basab's thesis off-centre
+  //[[ 0.27399398 -0.06762399  0.27399398]  
+  // [-0.06762399 -0.82547997 -0.06762399]
+  // [ 0.27399398 -0.06762399  0.27399398]]
+  
+  if(row > 1 && col > 1 && row < IMAGE_HEIGHT_m_2 && col < IMAGE_WIDTH_m_2){
     uint global_idx = row*IMAGE_WIDTH + col;
     int4 gt_thresh = 0, true_vec = 1;
     int4 curr, prev, diff, thre;
   
-  
-    curr = convert_int4(current[global_idx]);
+    int4 curr_conv_row0[2],
+         curr_conv_row1[2],
+         prev_conv_row2[2];
+    
+    curr_conv_row0[0] = convert_int4(current[global_idx - IMAGE_WIDTH]);
+    curr_conv_row0[1] = convert_int4(current[global_idx - IMAGE_WIDTH_p_1]);
+    curr_conv_row1[0] = convert_int4(current[global_idx]);
+    curr_conv_row1[1] = convert_int4(current[global_idx + 1]);
+    curr_conv_row1[0] = convert_int4(current[global_idx + IMAGE_WIDTH]);
+    curr_conv_row1[1] = convert_int4(current[global_idx + IMAGE_WIDTH_p_1]);
+    
+    float sum = 0;
+    sum =  curr_conv_row0[0].x*(-0.27399398) + curr_conv_row0[0].y*(0.06762399) + curr_conv_row0[0].z*(-0.27399398);
+    sum += curr_conv_row1[0].x*(0.06762399)  + curr_conv_row1[0].y*(0.82547997) + curr_conv_row1[0].z*(0.06762399);
+    sum += prev_conv_row2[0].x*(-0.27399398) + prev_conv_row2[0].y*(0.06762399) + prev_conv_row2[0].z*(-0.27399398);
+    curr.x = sum;
+    
+    
+    sum = 0;
+    sum =  curr_conv_row0[0].y*(-0.27399398) + curr_conv_row0[0].z*(0.06762399) + curr_conv_row0[0].w*(-0.27399398);
+    sum += curr_conv_row1[0].y*(0.06762399)  + curr_conv_row1[0].z*(0.82547997) + curr_conv_row1[0].w*(0.06762399);
+    sum += prev_conv_row2[0].y*(-0.27399398) + prev_conv_row2[0].z*(0.06762399) + prev_conv_row2[0].w*(-0.27399398);
+    curr.y = sum;
+    
+    
+    sum = 0;
+    sum =  curr_conv_row0[0].z*(-0.27399398) + curr_conv_row0[0].w*(0.06762399) + curr_conv_row0[1].x*(-0.27399398);
+    sum += curr_conv_row1[0].z*(0.06762399)  + curr_conv_row1[0].w*(0.82547997) + curr_conv_row1[1].x*(0.06762399);
+    sum += prev_conv_row2[0].z*(-0.27399398) + prev_conv_row2[0].w*(0.06762399) + prev_conv_row2[1].x*(-0.27399398);
+    curr.z = sum;
+    
+    
+    sum = 0;
+    sum =  curr_conv_row0[0].w*(-0.27399398) + curr_conv_row0[1].x*(0.06762399) + curr_conv_row0[1].y*(-0.27399398);
+    sum += curr_conv_row1[0].w*(0.06762399)  + curr_conv_row1[1].x*(0.82547997) + curr_conv_row1[1].y*(0.06762399);
+    sum += prev_conv_row2[0].w*(-0.27399398) + prev_conv_row2[1].x*(0.06762399) + prev_conv_row2[1].y*(-0.27399398);
+    curr.w = sum;
+    
+
     prev = convert_int4(previous[global_idx]);
     thre = convert_int4(threshold[global_idx]);
     diff = curr - prev;
@@ -126,9 +166,44 @@ __kernel void difference(__global uchar4* current, __global uchar4* previous,
   }
 }
 """
+    #~ kernel = """
+#~ #pragma OPENCL EXTENSION cl_amd_media_ops : enable
+#~ 
+#~ __kernel void difference(__global uchar4* current, __global uchar4* previous,
+                         #~ __global uchar4* threshold, __global short4*  output){
+  #~ uint row = get_global_id(1), col = get_global_id(0);
+#~ 
+#~ 
+  #~ if(row < IMAGE_HEIGHT_m_2 && col < IMAGE_WIDTH_m_2){
+    #~ uint global_idx = row*IMAGE_WIDTH + col;
+    #~ int4 gt_thresh = 0, true_vec = 1;
+    #~ int4 curr, prev, diff, thre;
+  #~ 
+    #~ curr = convert_int4(current[global_idx]);
+    #~ prev = convert_int4(previous[global_idx]);
+    #~ thre = convert_int4(threshold[global_idx]);
+    #~ diff = curr - prev;
+    #~ 
+    #~ gt_thresh = diff*diff > thre*thre ? true_vec : gt_thresh;
+    #~ 
+    #~ diff = gt_thresh > 0 ? diff : 0;
+    #~ 
+    #~ thre = gt_thresh > 0 ? thre + THRESHOLD_RATE_UP : thre - THRESHOLD_RATE_DOWN;
+    #~ thre = thre > MAX_THRESHOLD ? MAX_THRESHOLD : thre;
+    #~ thre = thre < MIN_THRESHOLD ? MIN_THRESHOLD : thre;
+#~ 
+    #~ output[global_idx]    = convert_short4(diff);
+    #~ threshold[global_idx] = convert_uchar4(thre);
+    #~ previous[global_idx]  = convert_uchar4(curr);
+  #~ }
+#~ }
+#~ """
     kernel = kernel.replace("IMAGE_SIZE", str(self.image_height*self.image_width))
+    kernel = kernel.replace("IMAGE_WIDTH_p_1", str(self.image_width + 1))
+    kernel = kernel.replace("IMAGE_WIDTH_t_2_p_1", str(self.image_width*2 + 1))
+    kernel = kernel.replace("IMAGE_HEIGHT_m_2", str(self.image_height - 2))
+    kernel = kernel.replace("IMAGE_WIDTH_m_2", str(self.image_width - 2))
     kernel = kernel.replace("IMAGE_HEIGHT", str(self.image_height))
-    kernel = kernel.replace("IMAGE_WIDTH_4", str(self.image_width))
     kernel = kernel.replace("IMAGE_WIDTH", str(self.image_width))
     kernel = kernel.replace("THRESHOLD_RATE_UP", str(self.threshold_rate))
     kernel = kernel.replace("THRESHOLD_RATE_DOWN", str(self.threshold_rate))
