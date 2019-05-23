5190327e2a14c4313ee1d3a59ce3d3ec357efdc1
chanokin <chanokin@gmail.com>
2019-05-23 21:40:07 +0100

    initialized stuff

--- pydvs/cpp/dvs_emu.cpp
+++ pydvs/cpp/dvs_emu.cpp
@@ -1,9 +1,5 @@
 #include "dvs_emu.hpp"
 
-PyDVS::PyDVS(){
-
-}
-
 PyDVS::PyDVS(const size_t w, const size_t h, const size_t fps){
     _w = w;
     _h = h;
@@ -16,45 +12,90 @@ PyDVS::~PyDVS(){
     }
 }
 
-bool PyDVS::init(const int cam_id){
+bool PyDVS::init(const int cam_id, const float relaxRate, 
+        const float adaptUp, const float adaptDown){
     _cam = VideoCapture(cam_id);
     _open = _cam.isOpened();
     if(!_open){
+        cerr << "Error. Cannot open video feed!" << endl;
         return false;
     }
-
+    
+    bool success = true;
+    _is_vid = false;
     if(_w == 0 || _h == 0 || _fps == 0){
         _get_size();
         _get_fps();
     } else {
-
+        success &= _set_size();
+        success &= _set_fps();
     }
-    return true;
+
+    _init_matrices();
+    _set_adapt(relaxRate, adaptUp, adaptDown);
+    return success;
 }
 
-bool PyDVS::init(const String& filename){
+bool PyDVS::init(const String& filename, const float relaxRate, 
+        const float adaptUp, const float adaptDown){
     _cam = VideoCapture(filename);
     _open = _cam.isOpened();
     if(!_open){
+        cerr << "Error. Cannot open video feed!" << endl;
         return false;
     }
 
+    _is_vid = true;
     _get_size();
     _get_fps();
+    _init_matrices();
+    _set_adapt(relaxRate, adaptUp, adaptDown);
 
     return true;
 }
 
 
-PyDVS::_get_size(){
+void PyDVS::_get_size(){
     if(_open){
         _w = _cam.get(cv::CV_CAP_PROP_FRAME_WIDTH);
         _h = _cam.get(cv::CV_CAP_PROP_FRAME_HEIGHT);
     }
 }
 
-PyDVS::_get_fps(){
+bool PyDVS::_set_size(){
+ 
+    if(_open){
+        size_t w = _cam.get(cv::CV_CAP_PROP_FRAME_WIDTH);
+        size_t h = _cam.get(cv::CV_CAP_PROP_FRAME_HEIGHT);
+        bool success = true;
+        success &= _cam.set(cv::CV_CAP_PROP_FRAME_WIDTH, _w);
+        success &= _cam.set(cv::CV_CAP_PROP_FRAME_HEIGHT, _h);
+
+        if (!success){
+            _cam.set(cv::CV_CAP_PROP_FRAME_WIDTH, w);
+            _cam.set(cv::CV_CAP_PROP_FRAME_HEIGHT, h);
+            _w = w; 
+            _h = h;
+        }
+        return success;
+    }
+}
+
+void PyDVS::_get_fps(){
     if(_open){
         _fps = _cam.get(cv::CV_CAP_PROP_FPS);
+    }
+}
+
+bool PyDVS::_set_fps(){
+    if(_open){
+        size_t fps = _cam.get(cv::CV_CAP_PROP_FPS);
+        bool success = true;
+        success &= _cam.set(cv::CV_CAP_PROP_FPS, _fps);
+        if(!success){
+            _cam.set(cv::CV_CAP_PROP_FPS, fps);
+            _fps = fps;
+        }
+        return success;
     }
 }
--- pydvs/cpp/dvs_emu.hpp
+++ pydvs/cpp/dvs_emu.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <stdint>
 #include "opencv2/opencv.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+// #include <opencv2/highgui.hpp>
 
 // using namespace cv;
 using namespace std;
@@ -15,10 +18,18 @@ public:
         _w(0), _h(0), _open(false);
     PyDVS(const size_t w, const size_t h, const size_t fps=0);
     ~PyDVS();
-    bool init(const int cam_id=0);
-    bool init(const String& filename);
+    bool init(const int cam_id=0, const float relaxRate=1.0f, 
+        const float adaptUp=1.0f, const float adaptDown=1.0f);
+    bool init(const String& filename, const float relaxRate=1.0f, 
+        const float adaptUp=1.0f, const float adaptDown=1.0f);
 
-
+    void inline setFPS(const size_t fps){_fps = fps;}
+    void inline setWidth(const size_t w){_w = w;}
+    void inline setHeight(const size_t h){_h = h;}
+    void inline setRelaxRate(const float r){_relaxRate = r;}
+    void inline setAdaptUp(const float u){_adaptUp = u;}
+    void inline setAdaptDown(const float d){_adaptDown = d;}
+    
 private:
     cv::VideoCapture _cam;
     cv::Mat _in;
@@ -32,9 +43,24 @@ private:
 
     size_t _w, _h;
     bool _open;
+    bool _is_vid;
+
     void _get_size();
     void _get_fps();
-
+    bool _set_size();
+    bool _set_fps();
+    inline void _set_adapt(const float relaxRate, const float adaptUp, 
+                            const float adaptDown){
+        _relaxRate = relaxRate;
+        _adaptUp = adaptUp;
+        _adaptDown = adaptDown;
+    }
+    inline void _init_matrices(){
+        // CV_32F 32-bit floating point numbers
+        _ref = Mat::zeros(_h, _w, CV_32F);
+        _diff = Mat::zeros(_h, _w, CV_32F);
+        _thr = Mat::zeros(_h, _w, CV_32F);
+    }
 };
 
 #endif //DVS_EMU_HPP
