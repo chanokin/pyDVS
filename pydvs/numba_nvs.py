d3fb4015ad101599e62f1de435a1358437c545c3
chanokin <chanokin@gmail.com>
2020-11-28 17:11:24 +0000

    works with numba, now to two-cell sim

--- pydvs/numba_nvs.py
+++ pydvs/numba_nvs.py
@@ -2,47 +2,49 @@ from numba import jit, float32
 import numpy as np
 
 
-# @jit(parallel=True, nopython=True)
+@jit(parallel=True, nopython=True)
 def thresholded_difference(
         frame: np.ndarray, reference: np.ndarray, thresholds: np.ndarray,
         spikes: np.ndarray):
-    active = np.zeros_like(frame, dtype='uint8')
+    active = np.zeros_like(frame)
 
     spikes[:] = frame - reference
     active[:] = (np.abs(spikes) >= thresholds)
     spikes *= active
-    # spikes[:] = np.round(spikes/thresholds)
+    # todo: since outputs are spikes, how do I discretize increments?
+    # is this the right way?
+    spikes[:] = (spikes // thresholds) * thresholds
 
     return active
 
 
-# @jit(nopython=True)
+@jit(nopython=True)
 def nvs_0(frame: np.ndarray, reference: np.ndarray, thresholds: np.ndarray,
           spikes: np.ndarray):
-    active = np.zeros_like(frame, dtype='uint8')
+    active = np.zeros_like(frame)
 
     active[:] = thresholded_difference(frame, reference, thresholds, spikes)
     reference[:] += spikes
 
-# @jit(nopython=True)
+
+@jit(nopython=True)
 def nvs_leaky(frame: np.ndarray, reference: np.ndarray, thresholds: np.ndarray,
               spikes: np.ndarray, reference_leak: np.float):
-    active = np.zeros_like(frame, dtype='uint8')
+    active = np.zeros_like(frame)
 
     active[:] = thresholded_difference(frame, reference, thresholds, spikes)
 
     # reference is a leaky integrator
-    # todo: since outputs are spikes, how do I discretize increments?
     reference[:] = reference * reference_leak + spikes
 
 
-# @jit(nopython=True)
+@jit(nopython=True)
 def nvs_leaky_adaptive(frame: np.ndarray, reference: np.ndarray,
                        thresholds: np.ndarray, spikes: np.ndarray,
                        reference_leak: np.float,
                        threshold_base: np.float,
                        threshold_mult_incr: np.float, threshold_leak: np.float):
-    active = np.zeros_like(frame, dtype='uint8')
+    active = np.zeros_like(frame)
 
     active[:] = thresholded_difference(frame, reference, thresholds, spikes)
 
@@ -53,7 +55,6 @@ def nvs_leaky_adaptive(frame: np.ndarray, reference: np.ndarray,
     )
 
     # reference is also a leaky integrator
-    # todo: since outputs are spikes, how do I discretize increments?
     reference[:] = (
             reference * reference_leak + spikes
     )
@@ -67,8 +68,8 @@ def nvs_noisy_leaky_adaptive(frame: np.ndarray, reference: np.ndarray,
                              threshold_base: np.float,
                              threshold_mult_incr: np.float, threshold_leak: np.float,
                              reference_leak_probability: np.float):
-    active = np.zeros_like(frame, dtype='uint8')
-    leak_active = np.zeros_like(frame)
+    active = np.zeros_like(frame)
+    leak_active = np.ones_like(frame)
 
     active[:] = thresholded_difference(frame, reference, thresholds, spikes)
 
@@ -79,8 +80,36 @@ def nvs_noisy_leaky_adaptive(frame: np.ndarray, reference: np.ndarray,
     )
 
     # reference is also a leaky integrator
-    # todo: since outputs are spikes, how do I discretize increments?
+    leak_active += (np.random.uniform(0., 1., size=frame.shape) <=
+                    reference_leak_probability) * (reference_leak - 1)
 
+    reference[:] = (
+            reference * leak_active + spikes
+    )
+
+
+@jit(nopython=True)
+def nvs_bi_noisy_leaky_adaptive(
+        frame: np.ndarray, reference: np.ndarray, thresholds: np.ndarray,
+        spikes: np.ndarray,
+        reference_leak: np.float, threshold_base: np.float,
+        threshold_mult_incr: np.float, threshold_leak: np.float,
+        reference_leak_probability: np.float,
+        reference_off: np.ndarray, thresholds_off: np.ndarray, target_off: np.float):
+    active = np.zeros_like(frame)
+    diff_off = np.zeros_like(frame)
+    active_off = np.zeros_like(frame)
+    leak_active = np.zeros_like(frame)
+
+    active[:] = thresholded_difference(frame, reference, thresholds, spikes)
+    active_off[:] = thresholded_difference(frame, reference, thresholds, spikes)
+    # thresholds as leaky integrators
+    thresholds[:] = (
+            threshold_base + (thresholds - threshold_base) * threshold_leak +
+            (thresholds - threshold_base) * threshold_mult_incr * active
+    )
+
+    # reference is also a leaky integrator
     leak_active[:] = (np.random.uniform(0., 1., size=frame.shape) <=
                       reference_leak_probability) * reference_leak
     leak_active[leak_active == 0] = 1.0
